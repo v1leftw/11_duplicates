5e2663a17a544c1410fb9f78155eb9e0b42eec0d
v1leftw <dmitry.g.fake@gmail.com>
2018-10-25 17:59:22 +0300

    [+] added functions to actually search duplicates

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # Anti-Duplicator
 
-[TODO. There will be project description]
+Script searches duplicate files in the directory.
+File marked as duplicate if it has same name and same size.
+To specify folder use -path argument
+
+# Quickstart
+Examples of script launch on Linux, Python 3.5
+
+```
+$ python duplicates.py -path f:\ccs\
+File: кб.gif. Size: 10833754. Copies at:
+-  f:\ccs\кб.gif
+-  f:\ccs\New folder\кб.gif
+-  f:\ccs\New folder\New folder\кб.gif
+-  f:\ccs\New folder\New folder\New folder\кб.gif
+
+```
 
 # Project Goals
 
--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,38 @@
+import os
+import argparse
+from collections import namedtuple, defaultdict
 
-if __name__ == '__main__':
-    pass
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Enter the path:")
+    parser.add_argument("-path", required=True, help="Path to file")
+    return parser.parse_args()
+
+
+def get_all_files(directory_path):
+    # based on https://stackoverflow.com/a/40061513/8482475
+    dict_of_files = defaultdict(list)
+    file_description = namedtuple("File", "Name Size")
+    for root, dirs, files in os.walk(directory_path, topdown=True):
+        for name in files:
+            path_to_file = os.path.join(root, name)
+            description = file_description(name, os.path.getsize(path_to_file))
+            # Creates dictionary where key stores description of the file
+            # and value stores all paths to this file
+            dict_of_files[description].append(path_to_file)
+    return dict_of_files
+
+
+def find_duplicates(file_list):
+    return {description: paths for (description, paths) in file_list.items()
+            if len(paths) > 1}
+
+
+if __name__ == "__main__":
+    path = get_args().path
+    duplicates = find_duplicates(get_all_files(path))
+    for file, file_paths in duplicates.items():
+        print("File: {}. Size: {}. Copies at: ".format(file.Name, file.Size))
+        for path in file_paths:
+            print("- ", path)
+        print("\t")  # file-divider
